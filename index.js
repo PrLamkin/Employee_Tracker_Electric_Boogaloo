f4bd7c699f75cd69c481cb51f011c856502a17ba
PrLamkin <shiektheimmortal@outlook.com>
2020-10-27 18:22:23 -0500

    this should be the proper step to set up a connection

--- index.js
+++ index.js
@@ -4,12 +4,32 @@ require("console.table")
 
 let connection = mysql.createConnection({
     host: "localhost",
-    port: 3080,
+    port: 3306,
     user: "root",
     password: "7445",
     database: "employeeDB"
 });
 
 connection.connect(function(err) {
-    if (err) throw err
+    if (err) throw err;
+    firstPrompt();
 });
+
+function firstPrompt() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "task",
+            message: "Would you like to do?",
+            choices: [
+                "view Employees",
+                "View Employees by Department",
+                "Add Employee",
+                "Remove employee",
+                "Update Employee Role",
+                "add Role",
+                "End"
+            ]
+        })
+
+}
